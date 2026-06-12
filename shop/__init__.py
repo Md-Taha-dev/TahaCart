b341e4780300e2f110d575d3f0e90a6699b95320
Md Taha <tahamohammed.ece@gmail.com>
2026-06-12 20:31:30 +0530

    Update __init__.py

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,15 +1,19 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
 from flask_login import LoginManager
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__,
     template_folder="../templates",
     static_folder="../static"
     )
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tahauchiha1717@localhost/shopnest'
-app.config['SECRET_KEY']='7f590dae42a606b1adf503c4'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:tahauchiha1717@localhost/shopnest')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '7f590dae42a606b1adf503c4')
 db=SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager=LoginManager(app)
